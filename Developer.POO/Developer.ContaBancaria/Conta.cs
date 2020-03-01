539715e1b6007c44f48d3b725c1407aedd232277
fernandolucasgsouza <fernandolucass.2012@gmail.com>
2020-03-01 11:18:57 -0300

    conta bancaria POO

---
+++ Developer.POO/Developer.ContaBancaria/Conta.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Developer.ContaBancaria
+{
+    public class Conta
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public decimal Saldo { get; private set; }
+
+        public Conta(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public Conta(int numero, string titular, decimal saldo) : this(numero, titular)
+        {
+            Saldo = saldo;
+        }
+
+        public void Deposito(decimal valor)
+        {
+            Saldo += valor;
+
+        }
+
+        public void Saque(decimal valor)
+        {
+            decimal taxa = 5;
+            Saldo -= valor + taxa;
+        }
+
+
+        public override string ToString()
+        {
+            return $"\nDados da Conta :\nConta: {Numero}, Titular: {Titular}, Saldo: R${Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+
+
+    }
+}
---
+++ Developer.POO/Developer.ContaBancaria/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace Developer.ContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Conta conta;
+
+            Console.Write("Entre com o número da conta: ");
+            int numero_conta = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+
+
+
+            Console.Write("Haverá deposito inicial (s/n): ");
+            char questao = char.Parse(Console.ReadLine().ToUpper());
+
+            if (questao == 'S')
+            {
+                Console.Write("Entre com o valor de deposito inicial: ");
+                decimal deposito1 = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Conta(numero_conta, titular, deposito1);
+            }
+            else
+            {
+                conta = new Conta(numero_conta, titular);
+            }
+            Console.WriteLine(conta);
+
+            Console.Write("Entre com o valor para depósito: ");
+            decimal deposito2 = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(deposito2);
+            Console.WriteLine(conta);
+
+            Console.Write("Entre com o valor de saque: ");
+            decimal valorSaque = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(valorSaque);
+            Console.WriteLine(conta);
+
+
+        }
+    }
+}
