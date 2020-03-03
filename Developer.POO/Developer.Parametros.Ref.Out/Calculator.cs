9775033ae68d714f1255ce31b0a242b161e99cd9
fernandolucasgsouza <fernandolucass.2012@gmail.com>
2020-03-02 22:25:32 -0300

    parametros ref e out

---
+++ Developer.POO/Developer.Parametros.Ref.Out/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Developer.Parametros.Ref.Out
+{
+    public class Calculator
+    {
+
+        public static double TripleRef(ref double value)
+        {
+            return value *= 3;
+        }
+
+        public static double TripleOut(double origin, out double result)
+        {
+            result = origin * 3;
+            return result;
+        }
+    }
+}
---
+++ Developer.POO/Developer.Parametros.Ref.Out/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Developer.Parametros.Ref.Out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double valueA = 3;
+            Calculator.TripleRef(ref valueA);
+            Console.WriteLine($"Valor triplicado: {valueA}");
+
+            double tripleB;
+            Calculator.TripleOut(10, out tripleB);
+            Console.WriteLine($"Valor triplicado: {tripleB}");
+        }
+    }
+}
