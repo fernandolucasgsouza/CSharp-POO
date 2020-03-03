708bcc8b2daf5b2745d833e7793a11695235a2ed
fernandolucasgsouza <fernandolucass.2012@gmail.com>
2020-03-02 22:05:36 -0300

    parametros dinamicos

---
+++ Developer.POO/Developer.Parametros.Dinamicos/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace Developer.Parametros.Dinamicos
+{
+    public static class Calculator
+    {
+
+        public static double Sum(params double[] values)
+        {
+            double sum = 0;
+            for (int i = 0; i < values.Length; i++)
+                sum += values[i];
+
+            return sum;
+        }
+    }
+}
---
+++ Developer.POO/Developer.Parametros.Dinamicos/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Developer.Parametros.Dinamicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double soma = Calculator.Sum(1,2,3,4);
+            Console.WriteLine($"Soma = {soma}");
+        }
+    }
+}
